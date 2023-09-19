407f0c65e885571eb153a44c481125856454f029
Joshua Mclean <joshmcleana@gmail.com>
2023-09-18 23:11:18 -0700

    built out ExpenseItem a bit more...

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,3 +1,11 @@
 export default function ExpenseItem() {
-    return <h2>Expense Item</h2>
+  return (
+    <>
+      <div>March, 28th, 2023</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </>
+  );
 }
