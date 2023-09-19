47eb750a260f43f7700ba11c0b0b6b5e851aca54
Joshua Mclean <joshmcleana@gmail.com>
2023-09-18 23:24:44 -0700

    Added variables for Expense Items

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import './ExpenseItem.css';
+const expenseDate = new Date(2023, 9, 18);
+const expenseTitle = 'Car Insurance';
+const expenseAmount = 294.67;
 
 export default function ExpenseItem() {
   return (
     <div className='expense-item'>
-      <div>March, 28th, 2023</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
       </div>
     </div>
   );
