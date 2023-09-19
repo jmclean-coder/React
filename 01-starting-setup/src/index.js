71d1babc67675363c5c228d581cfdb2c6f628445
Joshua Mclean <joshmcleana@gmail.com>
2023-09-18 22:43:09 -0700

    add note to index.js

--- 01-starting-setup/src/index.js
+++ 01-starting-setup/src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// identifying the root in our webpage, telling react where to inject our React App
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
