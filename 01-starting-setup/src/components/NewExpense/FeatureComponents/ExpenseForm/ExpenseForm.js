7f96bdb0f4dd13338e8c08730a78079a5c92b626
Joshua Mclean <joshmcleana@gmail.com>
2023-09-27 16:53:55 -0700

    prefer formData object and two way binding

--- 01-starting-setup/src/components/NewExpense/FeatureComponents/ExpenseForm/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/FeatureComponents/ExpenseForm/ExpenseForm.js
@@ -2,57 +2,74 @@ import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    // 1: Individual functions for each field
-    const [inputAmount, setInputAmount] = useState('');
-    const [inputDate, setInputDate] = useState('');
-    const [inputTitle, setInputTitle] = useState('');
-
     // 2: Single object FormData State
-    // const [formData, setFormData] = useState({
-    //     inputAmount: '',
-    //     inputDate: '',
-    //     inputTitle: '',
-    // });
+    const [formData, setFormData] = useState({
+        inputAmount: '',
+        inputDate: '',
+        inputTitle: '',
+    });
+    // 1: Individual functions for each field
+    // const [inputAmount, setInputAmount] = useState('');
+    // const [inputDate, setInputDate] = useState('');
+    // const [inputTitle, setInputTitle] = useState('
     // Whenever I have a state update that depends on the previous state, I should use the arror function syntax that consumes the "prevState"
 
-    // const handleAmountChange = (e) => {
-    //     console.log(e.target.value)
-    //     setFormData((prevState) => {
-    //         return { ...prevState, inputAmount: e.target.value }
-    //     });
-    // };
-    // const handleDateChange = (e) => {
-    //     console.log(e.target.value)
-    //     setFormData((prevState) => {
-    //         return { ...prevState, inputDate: e.target.value }
-    //     });
-    // };
-    // const handleTitleChange = (e) => {
-    //     console.log(e.target.value)
-    //     setFormData((prevState => {
-    //         return {...prevState, inputTitle: e.target.value }
-    //     }));
-    // };
+    const handleAmountChange = (e) => {
+        setFormData((prevState) => {
+            return { ...prevState, inputAmount: e.target.value }
+        });
+    };
+    const handleDateChange = (e) => {
+        setFormData((prevState) => {
+            return { ...prevState, inputDate: e.target.value }
+        });
+    };
+    const handleTitleChange = (e) => {
+        setFormData((prevState => {
+            return {...prevState, inputTitle: e.target.value }
+        }));
+    };
 
     // 3: Generalized change handler with manual flow control mapping
-    const inputChangeHandler = (identifier, value) => {
-        if (identifier === 'amount') {
-            setInputAmount(value);
-        } else if (identifier === 'date') {
-            setInputDate(value);
-        } else if (identifier === 'title') {
-            setInputTitle(value);
-        }
-    }
+    // const inputChangeHandler = (identifier, value) => {
+    //     if (identifier === 'amount') {
+    //         setInputAmount(value);
+    //     } else if (identifier === 'date') {
+    //         setInputDate(value);
+    //     } else if (identifier === 'title') {
+    //         setInputTitle(value);
+    //     }
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        const { inputAmount, inputTitle, inputDate } = formData;
+        const submittedData = {
+            amount: inputAmount,
+            date: new Date(inputDate),
+            title: inputTitle,
+        };
+
+        setFormData({
+            inputAmount: '',
+            inputDate: '',
+            inputTitle: '',
+        });
+
+        console.log(submittedData);
+    };
+
+
+
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Amount</label>
           <input
-        //   onChange={handleAmountChange} /** 2 */
-          onChange={ (e) => { inputChangeHandler("amount",e.target.value) } } /** 3 */
+          value={formData.inputAmount}
+          onChange={handleAmountChange} /** 2 */
+        //   onChange={ (e) => { inputChangeHandler("amount",e.target.value) } } /** 3 */
           type="number"
           min="0.01"
           step="0.01" />
@@ -60,8 +77,9 @@ const ExpenseForm = () => {
         <div className="new-expense__control">
           <label>Date</label>
           <input
-        //   onChange={handleDateChange} /** 2 */
-          onChange={ (e) => { inputChangeHandler("date",e.target.value) } } /** 3 */
+          value={formData.inputDate}
+          onChange={handleDateChange} /** 2 */
+        //   onChange={ (e) => { inputChangeHandler("date",e.target.value) } } /** 3 */
           type="date"
           min="2019-01-01"
           max="2025-12-31" />
@@ -69,8 +87,9 @@ const ExpenseForm = () => {
         <div className="new-expense__control">
           <label>Title</label>
           <input
-        //   onChange={handleTitleChange} /** 2 */
-          onChange={ (e) => { inputChangeHandler("title",e.target.value) } } /** 3 */
+          value={formData.inputTitle}
+          onChange={handleTitleChange} /** 2 */
+        //   onChange={ (e) => { inputChangeHandler("title",e.target.value) } } /** 3 */
           type="text" />
         </div>
       </div>
