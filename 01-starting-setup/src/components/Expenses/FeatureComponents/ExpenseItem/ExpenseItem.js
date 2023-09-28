d62ec745d6ee850ae06cd40e408fc36a03bb6ff2
Joshua Mclean <joshmcleana@gmail.com>
2023-09-28 01:00:44 -0700

    remove to make it a dumb component again

--- 01-starting-setup/src/components/Expenses/FeatureComponents/ExpenseItem/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/FeatureComponents/ExpenseItem/ExpenseItem.js
@@ -4,20 +4,14 @@ import ExpenseDate from "components/Expenses/FeatureComponents/ExpenseDate/Expen
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ date, title, amount }) => {
-  const [newTitle, setNewTitle ] = useState(title);
 
-  const clickHandler = () => {
-    setNewTitle('Updated!');
-    console.log(title);
-  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{newTitle}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
