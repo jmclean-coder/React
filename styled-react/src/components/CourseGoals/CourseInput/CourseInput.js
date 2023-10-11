79cafdf38239b209de8a41faf1e3c59bea60497f
Joshua Mclean <joshmcleana@gmail.com>
2023-10-11 16:52:57 -0700

    Finished Styled Components Examples

--- styled-react/src/components/CourseGoals/CourseInput/CourseInput.js
+++ styled-react/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,34 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
 import "./CourseInput.css";
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${(props) => (props.invalid ? "red" : "black")};
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+    background: ${(props) => (props.invalid ? "#f79c91c0" : "transparent")};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #ffffff;
+    border-color: #8b005d;
+  }
+`;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -30,13 +57,11 @@ const CourseInput = (props) => {
   return (
     <form onSubmit={formSubmitHandler}>
       {/* Dynamic classes = lightweight! */}
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      {/* <FormControl  className={!isValid && 'invalid'}> dynamic class */}
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
-        <input
-          type="text"
-          onChange={goalInputChangeHandler}
-        />
-      </div>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
