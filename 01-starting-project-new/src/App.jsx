19820b3789941cf0a0b326d9996aff1056796508
Joshua Mclean <joshmcleana@gmail.com>
2023-11-06 07:38:46 -0800

    Use new components Demonstrate two basic ways to pass props

--- 01-starting-project-new/src/App.jsx
+++ 01-starting-project-new/src/App.jsx
@@ -1,12 +1,30 @@
 // import React from "react"; Not necessary to explicitly import react.
+import { CORE_CONCEPTS } from "./data";
+import CoreConceptCard from "./components/CoreConceptCard/CoreConceptCard";
 import Header from "./components/UI/Header/Header";
 
+const firstConcept = CORE_CONCEPTS[0];
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {/* note on approach... common method */}
+            <CoreConceptCard
+              image={firstConcept.image}
+              title={firstConcept.title}
+              description={firstConcept.description}
+            />
+
+            {/* shortened method using rest */}
+            <CoreConceptCard {...CORE_CONCEPTS[1]} />
+            <CoreConceptCard {...CORE_CONCEPTS[2]} />
+            <CoreConceptCard {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
