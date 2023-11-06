09dfe86e1112fe888fe84489444c249acd6d6cb6
Joshua Mclean <joshmcleana@gmail.com>
2023-11-06 04:32:12 -0800

    css modules examples

--- css-modules-react/src/components/UI/Button/Button.js
+++ css-modules-react/src/components/UI/Button/Button.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-import './Button.css';
+import styles from './Button.module.css'; // For CSS Modules
+
+// under the hood, this concept of CSS modules at build will take the classes imported to the component and make uniqe version of those classes for this component.
+// This is another way of scoping your css styles to a specific components
+// It can be the best of both world, we have our styles in css modules AND we have them scope to the component that uses those styles.
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
