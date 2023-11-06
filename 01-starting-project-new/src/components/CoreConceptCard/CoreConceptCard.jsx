9c6564fd02138a5b4e83093fa3a86c95331cab6a
Joshua Mclean <joshmcleana@gmail.com>
2023-11-06 07:38:20 -0800

    Make new components

---
+++ 01-starting-project-new/src/components/CoreConceptCard/CoreConceptCard.jsx
@@ -0,0 +1,8 @@
+import './CoreConceptCard.css'
+export default function CoreConceptCard({image, title, description}) {
+  return <li>
+    <img src={image} alt={title}/>
+    <h3>{title}</h3>
+    <p>{description}</p>
+  </li>
+}
--- 01-starting-project-new/src/components/UI/Header/Header.jsx
+++ 01-starting-project-new/src/components/UI/Header/Header.jsx
@@ -1,10 +1,21 @@
+import reactImg from '../../../assets/react-core-concepts.png'; // this is better for React optimizations
+import './Header.css'
+
 function Header() {
+
+  const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+  }
+
+  const description = reactDescriptions[getRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
