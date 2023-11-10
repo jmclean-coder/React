2db5544a7e8350c94d09674f06e9b7736c9ba2cf
Joshua Mclean <joshmcleana@gmail.com>
2023-11-10 02:24:40 -0800

    Refactor to include reusable Section component

--- 01-starting-project-new/src/components/CoreConcept/CoreConcept.jsx
+++ 01-starting-project-new/src/components/CoreConcept/CoreConcept.jsx
@@ -1,5 +1,6 @@
-import CoreConceptCard from "../CoreConceptCard/CoreConceptCard"
+import CoreConceptCard from "../CoreConceptCard/CoreConceptCard";
 import { CORE_CONCEPTS } from "../../data";
+import Section from "../UI/Section/Section";
 
 export default function CoreConcept() {
   function renderConceptCards() {
@@ -8,24 +9,23 @@ export default function CoreConcept() {
     });
   }
 
-  return (
-    <section id="core-concepts">
-      <h2>Core Concepts</h2>
-      <ul>
-        {/*
-        note on approach... common method with one isolated data point
-          <CoreConceptCard
-            image={firstConcept.image}
-            title={firstConcept.title}
-            description={firstConcept.description}
-          />
+  /*
+  note on approach... common method with one isolated data point
+    <CoreConceptCard
+    image={firstConcept.image}
+    title={firstConcept.title}
+    description={firstConcept.description}
+    />
 
-        shortened method using rest
-         <CoreConceptCard {...CORE_CONCEPTS[1]} />
-         <CoreConceptCard {...CORE_CONCEPTS[2]} />
-         <CoreConceptCard {...CORE_CONCEPTS[3]} /> */}
-        {renderConceptCards()}
-      </ul>
-    </section>
+  shortened method using rest
+    <CoreConceptCard {...CORE_CONCEPTS[1]} />
+    <CoreConceptCard {...CORE_CONCEPTS[2]} />
+    <CoreConceptCard {...CORE_CONCEPTS[3]} />
+*/
+
+  return (
+    <Section id="core-concepts" title="Core Concepts">
+      <ul>{renderConceptCards()}</ul>
+    </Section>
   );
 }
--- 01-starting-project-new/src/components/Examples/Examples.jsx
+++ 01-starting-project-new/src/components/Examples/Examples.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { EXAMPLES } from "../../data";
 import TabButton from "../UI/TabButton/TabButton";
 import CodeBlock from "../UI/CodeBlock/CodeBlock";
+import Section from "../UI/Section/Section";
+import Tabs from "../UI/Tabs/Tabs";
 
 export default function Examples() {
   // states
@@ -30,35 +32,39 @@ export default function Examples() {
     console.log(selectedTopic);
   }
   return (
-    <section id="examples">
-      <h2>Examples</h2>
-      <menu>
-        <TabButton
-          isSelected={selectedTopic === "components"}
-          onSelect={() => handleSelect("components")}
-        >
-          Components
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === "jsx"}
-          onSelect={() => handleSelect("jsx")}
-        >
-          JSX
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === "props"}
-          onSelect={() => handleSelect("props")}
-        >
-          Props
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === "state"}
-          onSelect={() => handleSelect("state")}
-        >
-          State
-        </TabButton>
-      </menu>
-      {tabContent}
-    </section>
+    <Section id="examples" title="Examples">
+      <Tabs
+        buttons={
+          <>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onClick={() => handleSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onClick={() => handleSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onClick={() => handleSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onClick={() => handleSelect("state")}
+            >
+              State
+            </TabButton>
+          </>
+        }
+      >
+        {tabContent}
+      </Tabs>
+    </Section>
   );
 }
