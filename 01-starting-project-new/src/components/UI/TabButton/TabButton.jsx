33645134a87118ae3a24d6f2ccfe4f4841abf537
Joshua Mclean <joshmcleana@gmail.com>
2023-11-10 02:26:23 -0800

    Add todo

--- 01-starting-project-new/src/components/UI/TabButton/TabButton.jsx
+++ 01-starting-project-new/src/components/UI/TabButton/TabButton.jsx
@@ -1,6 +1,7 @@
 export default function TabButton({ children, isSelected, ...props }) { // Forwarded props!
     console.log('TabButton Component rendering');
 
+    // todo revisit this to make it a very flexible button? How can this be specific to Tabs?
   return (
     <li>
       <button className={isSelected ? 'active' : ''} {...props}>{children}</button>
