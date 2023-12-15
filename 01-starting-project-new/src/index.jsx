a9fc8d7ae815f71c3b2dce97b52cd393987f37c4
Joshua Mclean <joshmcleana@gmail.com>
2023-12-15 00:05:17 -0800

    switch from <App /> to <Root />

--- 01-starting-project-new/src/index.jsx
+++ 01-starting-project-new/src/index.jsx
@@ -1,7 +1,21 @@
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom"
 import "./index.css";
+import Root from "./routes/Root.jsx";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+    },
+  ]);
 
 const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
+
+// ReactDOM.createRoot(entryPoint).render(<App />);
+ReactDOM.createRoot(entryPoint).render(<Root />);
