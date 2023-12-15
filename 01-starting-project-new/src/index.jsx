41033b28e18ce61cefb73d9f79b5f1d972749e85
Joshua Mclean <joshmcleana@gmail.com>
2023-12-14 22:31:47 -0800

    Revert "Add basic React Router Config"
    This reverts commit ec07edb13fa8c9968af546d1251ec4aa8e51c33d.

--- 01-starting-project-new/src/index.jsx
+++ 01-starting-project-new/src/index.jsx
@@ -1,24 +1,7 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App.jsx";
-// import "./index.css";
-import "./root.css";
-import Root from "./routes/root.jsx";
-import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
+import ReactDOM from "react-dom/client";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root />,
-        // element: <App />
-        errorElement: <ErrorPage />
-    }
-])
+import App from "./App.jsx";
+import "./index.css";
 
 const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(
-    <React.StrictMode>
-        <RouterProvider router={router}/>
-    </React.StrictMode>
-);
+ReactDOM.createRoot(entryPoint).render(<App />);
