6a78195d060a83f7d9f792013f24d32caaa7e624
Joshua Mclean <joshmcleana@gmail.com>
2024-01-05 16:37:12 -0800

    handle Restart derive active player Clean up state Handle draw Handle player name change

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,17 +1,130 @@
+import { useState } from "react";
 import Player from "./components/Player/Player";
+import GameBoard from "./components/GameBoard/GameBoard";
+import Log from "./components/Log/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+import GameOver from "./components/GameOver/GameOver";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+
+  return currentPlayer
+}
+
 
 function App() {
+  // const [activePlayer, setActivePlayer] = useState("X");
+  const [gameTurns, setGameTurns] = useState([]); // -----> Using a single piece of state to update multiple dependent components
+  /**
+   * instead of managing extra state,
+   * we derive the player from game turns state, which stores player info
+   */
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  const [players, setPlayers] = useState({
+    X: 'Player 1',
+    O: 'Player 2'
+});
+
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
+  for (const turn of gameTurns) {
+      const { square, player} = turn;
+      const { row, col } = square;
+
+      gameBoard[row][col] = player;
+
+  }
+
+  let winner = null;
+
+  /**
+   * Looping step by step over all winning combinations, extract the squares
+   */
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    console.table(firstSquareSymbol, secondSquareSymbol, thirdSquareSymbol);
+
+    if (firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+      ) {
+        winner = players[firstSquareSymbol];
+    }
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner
+
+  const handleSelectSquare = (rowIndex, colIndex) => {
+
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
+      const updatedTurns = [
+        {
+          square: {
+            row: rowIndex,
+            col: colIndex,
+          },
+          player: currentPlayer,
+        },
+        ...prevTurns,
+      ];
+      return updatedTurns;
+    });
+  };
+
+  function handleRematch() {
+    setGameTurns([])
+  }
+
+  function handlePlayerNameChage(symbol, newName) {
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName
+      };
+    });
+  }
+
   return (
     <main>
       <div id="game-container">
         {/* Players */}
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+            onChangeName={handlePlayerNameChage}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+            onChangeName={handlePlayerNameChage}
+          />
         </ol>
-        Game Board
+        {(winner || hasDraw) && <GameOver winner={winner} onRematch={handleRematch}/>}
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+          turns={gameTurns}
+          board={gameBoard}
+        />
       </div>
-      LOG
+      <Log turns={gameTurns} />
     </main>
   );
 }
