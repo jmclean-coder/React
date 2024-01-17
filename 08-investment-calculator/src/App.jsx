23b0a5bc74b8450ff22351001f91d65b446d7e05
Joshua Mclean <joshmcleana@gmail.com>
2024-01-16 16:19:38 -0800

    Built initial header component in App.jsx

--- 08-investment-calculator/src/App.jsx
+++ 08-investment-calculator/src/App.jsx
@@ -1,6 +1,11 @@
+import logo from './assets/investment-calculator-logo.png';
+
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
+    <header id='header'>
+      <img src={logo} alt='Investment Calculator Moneybag Logo'/>
+      <h1>React Investment Calculator</h1>
+    </header>
   )
 }
 
