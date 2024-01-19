8d271437f7549bc16b840904e7ab9880fac70754
Joshua Mclean <joshmcleana@gmail.com>
2024-01-19 13:07:20 -0800

    Spec'd out Input group

--- 08-investment-calculator/src/App.jsx
+++ 08-investment-calculator/src/App.jsx
@@ -4,6 +4,27 @@ const App = () => {
   return (
     <>
     <Header />
+    <div id='user-input'>
+      <div className='input-group'>
+      <label>Initial Invesment</label>
+      <input type="number"></input>
+      </div>
+
+      <div className='input-group'>
+      <label>Annual Investment</label>
+      <input type="number"></input>
+      </div>
+
+      <div className='input-group'>
+      <label>Expected Return</label>
+      <input type="number"></input>
+      </div>
+
+      <div className='input-group'>
+      <label>Duration</label>
+      <input type="number"></input>
+      </div>
+    </div>
     </>
   )
 }
