1291512c07b9fdc4e52b49f58af4806cb507066a
Joshua Mclean <joshmcleana@gmail.com>
2024-01-26 04:00:23 -0800

    remove unused className

--- 02-styling-updated/src/components/AuthInputs/AuthInputs.jsx
+++ 02-styling-updated/src/components/AuthInputs/AuthInputs.jsx
@@ -23,7 +23,7 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <div className="controls">
-        <p className='paragraph'>
+        <p>
           <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</label>
           <input
             type="email"
